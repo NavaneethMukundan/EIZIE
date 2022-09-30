305f34426888b04caeb7ef3435d88c79cec7e416
Navaneeth Mukundan <90769961+NavaneethMukundan@users.noreply.github.com>
2022-09-30 20:26:55 +0530

    Intal Commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const mainColor = Color(0xFF531ed2);
+const kBlack = Colors.black;
+const kWhite = Colors.white;
+const kGrey = Color.fromARGB(255, 213, 212, 212);
