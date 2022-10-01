61ef703fb2452d33785bc938e9198d2b906e2699
aneesh <aneesh172.ant@gmail.com>
2022-10-01 16:08:21 +0530

    appbar testing

--- lib/features/event/view/activity.dart
+++ lib/features/event/view/activity.dart
@@ -6,7 +6,27 @@ class EventScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        title: Image.asset('assets/images/2 1.png'),
+        actions: [
+          InkWell(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                  'assets/icons/Blue Colorful Abstract 3D Elements Kids Photo Collage 1.png'),
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {},
+          ),
+        ],
+      ),
       body: SafeArea(child: Center(child: Text('Event Screen'))),
     );
   }
