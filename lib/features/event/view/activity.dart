19eea245191e49efb8a1b6b72e53947f80ce3cc2
aneesh <aneesh172.ant@gmail.com>
2022-10-01 18:46:26 +0530

    appbar

--- lib/features/event/view/activity.dart
+++ lib/features/event/view/activity.dart
@@ -1,3 +1,5 @@
+import 'package:eizie/features/event/widget/eventappbar.dart';
+import 'package:eizie/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class EventScreen extends StatelessWidget {
@@ -6,27 +8,7 @@ class EventScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {},
-        ),
-        title: Image.asset('assets/images/2 1.png'),
-        actions: [
-          InkWell(
-            onTap: () {},
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset(
-                  'assets/icons/Blue Colorful Abstract 3D Elements Kids Photo Collage 1.png'),
-            ),
-          ),
-          IconButton(
-            icon: const Icon(Icons.notifications),
-            onPressed: () {},
-          ),
-        ],
-      ),
+      appBar: appbarwidget(),
       body: SafeArea(child: Center(child: Text('Event Screen'))),
     );
   }
